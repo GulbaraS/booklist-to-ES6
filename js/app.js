1fe4949ba102290735dc0565b155114b0dcc6d75
Gulbara Satybekova <gulbara.satybekova.on@gmail.com>
2022-02-15 12:34:36 +0600

    add book constructor and UI prototype

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
+// article about static
+//https://learn.javascript.ru/static-properties-methods
+
+
+
 /* Book Constructor */
-function Book() {}
+function Book(name, author, isbn) {
+  this.name = name;
+  this.author = author;
+  this.isbn = isbn;
+};
+
 
 /* UI Constructor  */
 function UI() {}
@@ -7,9 +17,19 @@ function UI() {}
 /* UI Prototype Methods */
 UI.prototype.addBookToList = function (book) {
   // Find the table
+  const list = document.querySelector("#book-list");
+
   // Create tr element
+  const row = document.createElement("tr");
+
   // Insert cols
+  row.innerHTML =
+    `<td>${book.title}</td>
+     <td>${book.author}</td>
+     <td>${book.isbn}</td>`;
+
   // Append to table
+  list.appendChild(row);
 };
 
 // Clear fields
@@ -22,4 +42,13 @@ document.getElementById("book-form").addEventListener("submit", (e) => {
   // Instance of UI
   // Add book to list
   // Clear fields
+
+  // Prevent actual submit
+  e.preventDefault();
 });
+
+
+//examples
+const potter = new Book("Harry Potter", "J. K. Rowling", 12345);
+
+console.log(potter);
